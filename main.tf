b2670cbb2600513f80d02166c5206116164513c6
Nurudeen25 <67113867+Nurudeen25@users.noreply.github.com>
2023-10-03 11:58:18 -0500

    25 restructure root module (#26)
    * #23 wip
    
    * Create .keep
    
    * Create week0
    
    * Update week0
    
    * Update README.md
    
    * Rename week0 to week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Update week0.md
    
    * Create week1.md
    
    * Update README.md
    
    * Update week1.md
    
    * Update week1.md
    
    * Update week1.md
    
    * WIP
    
    * #25 change structure of root module and add tagging to bucket

--- main.tf
+++ main.tf
@@ -1,39 +1,3 @@
-terraform {
-  # backend "remote" {
-  #   hostname = "app.terraform.io"
-  #   organization = "deen-terraform-beginner-bootcamp-2023"
-
-  #   workspaces {
-  #     name = "terra-house-1"
-  #   }
-  # }
-  cloud {
-    organization = "deen-terraform-beginner-bootcamp-2023"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-
-provider "aws" {
-}
-
-provider "random" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower            = true
@@ -47,8 +11,9 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming RAULES
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
